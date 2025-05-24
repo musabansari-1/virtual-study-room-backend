f165cd0e04dff2a73ad15ec90ba79c2047145e9c
Musab Zahid Ansari <musabzahida@gmail.com>
2025-05-25 01:09:34 +0530

    Changed cors origin

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ async def log_requests(request: Request, call_next):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://virtual-study-room-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
